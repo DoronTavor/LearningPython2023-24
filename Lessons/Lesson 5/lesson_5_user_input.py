badd9077d709bed710d8915162186d6c945e9382
DoronTavor <tavor.doron@gmail.com>
2023-12-22 14:32:29 +0200

    Lesson 5-User Inputs

---
+++ Lessons/Lesson 5/lesson_5_user_input.py
@@ -0,0 +1,35 @@
+import sys
+import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("")
+player_choice = input("Enter... \n1 for Rock, "
+                      "\n2 for Paper, \n3 for Scissors:\n\n")
+player = int(player_choice)
+if player < 1 or player > 3:
+    sys.exit("You have entered an invalid value, 1,2,or 3")
+
+computer_choice = random.choice("123")
+computer = int(computer_choice)
+print("")
+print("You chose: " + str(RPS(player)).replace('RPS.',".") + ".")
+print("Python chose: " + str(RPS(computer)).replace('RPS.',".") + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("🍾You win!")
+elif player == 2 and computer == 1:
+    print("🍾You win!")
+elif player == 3 and computer == 2:
+    print("🍾You win!")
+elif player == computer:
+    print("😲Tie game! ")
+else:
+    print("🐍Python wins!")
