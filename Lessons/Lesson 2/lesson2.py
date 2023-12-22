90b03b1736dbb99f11288995c3c41533c1d26787
DoronTavor <tavor.doron@gmail.com>
2023-12-22 12:21:20 +0200

    Lesson 2

---
+++ Lessons/Lesson 2/lesson2.py
@@ -0,0 +1,10 @@
+line01 = "********************"  # Header/Footer
+line02 = "*                  *"  # Re-use
+line03 = "*     WELCOME!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
