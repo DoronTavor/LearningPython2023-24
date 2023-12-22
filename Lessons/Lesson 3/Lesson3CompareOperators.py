82ea1d8c440fcf3d44390da510b5a7b101c10f91
DoronTavor <tavor.doron@gmail.com>
2023-12-22 13:16:07 +0200

    Lesson 3- Operators

---
+++ Lessons/Lesson 3/Lesson3CompareOperators.py
@@ -0,0 +1,10 @@
+x = True
+y = False
+z = True
+print(not x)
+print(not y)
+print(x and y)
+print(y and x)
+print(x or y)
+print(x and z)
+print(z or y)
---
+++ Lessons/Lesson 3/lesson3Operators.py
@@ -0,0 +1,19 @@
+# Operators
+name = "Tavor"  # the = Operator
+print(2 + 2)  # the + Operator
+print(4 - 2)  # the - Operator
+print(2 * 2)  # the * Operator
+print(24 / 5)  # the / Operator
+print(24 // 5)  # the // Operator- round downside
+print(24 % 5)  # the % Operator
+print(5 ** 2)  # the ** Operator
+
+meaning = 42
+meaning += 1
+print(meaning)
+meaning *= 10
+print(meaning)
+meaning /= 10
+print(meaning)
+
+
